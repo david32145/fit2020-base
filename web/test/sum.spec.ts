ef36f408a9ff92ec34c9528729da594955b7444f
David <nascimento32145@gmail.com>
2021-03-06 16:01:27 -0300

    config deploy

--- web/test/sum.spec.ts
+++ web/test/sum.spec.ts
@@ -2,6 +2,6 @@ test('Sum 1 + 1 should return 2', () => {
   expect(1 + 1).toEqual(2)
 })
 
-test('Sum 1 + 1 should return 3', () => {
-  expect(1 + 1).toEqual(3)
+test('Sum 1 + 2 should return 3', () => {
+  expect(1 + 2).toEqual(3)
 })
